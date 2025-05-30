d646ec1b9450622a4e53aa528d332705c7e7c9e9
Tiago Barroso <tiago-barroso@hotmail.com>
2025-05-30 17:43:03 -0300

    Implementing the AI service

--- src/services/ai.ts
+++ src/services/ai.ts
@@ -1,4 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const OpenAI = require("openai");
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const getSnippetFromText = async (text: string) => {
-    return 'Sample result';
-} 
+    return await apiFetch(text);
+};
+
+const apiFetch = async (text: string) => {
+    const prompt = `Summarize the following text in less than 30 words:\n\n${text}`;
+
+    const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+        max_tokens: 128,
+        temperature: 0.5,
+    });
+    return completion.choices[0].message.content.trim();
+};
