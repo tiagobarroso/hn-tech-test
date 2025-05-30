79d5c650cf31ed04b907c6913d15b6afa3754b70
Tiago Barroso <tiago-barroso@hotmail.com>
2025-05-30 16:01:12 -0300

    Adding backend cors support

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import snippetsRouter from './snippets';
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/snippets', snippetsRouter);
 
 export default app;
