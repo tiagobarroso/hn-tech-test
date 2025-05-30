f4b22592570ff3d7a3d58af37cd2c2d4696b0acc
Tiago Barroso <tiago-barroso@hotmail.com>
2025-05-30 12:57:54 -0300

    Starting up mongoose connection from env var

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import app from './routes/app';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`API server running on port ${PORT}`);
-});
+const mongoUri = process.env.MONGODB_URI as string;
+
+mongoose.connect(mongoUri)
+  .then(() => {
+    console.log('MongoDB connected!');
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+  });
